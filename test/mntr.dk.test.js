47a48b78443dc31b2224a6cce21d63cf954c648e
Peter Müller <munter@fumle.dk>
2019-11-12 23:27:58 +0100

    Initial commit

---
+++ test/mntr.dk.test.js
@@ -0,0 +1,27 @@
+const AssetGraph = require('assetgraph');
+const webbundle = require('../lib/webbundle');
+
+describe('when bundling online pages', () => {
+  it('should bundle https://mntr.dk', async () => {
+    const graph = new AssetGraph({
+      root: 'https://mntr.dk/',
+      canonicalRoot: 'https://mntr.dk/'
+    });
+
+    await graph.loadAssets('/');
+    await graph.populate({
+      followRelations: {
+        type: {
+          $nin: [
+            'HtmlAnchor',
+            'HtmlAlternateLink',
+            'SourceMapFile',
+            'SourceMapSource'
+          ]
+        }
+      }
+    });
+
+    await webbundle(graph, 'mntr.dk.wbn');
+  });
+});
